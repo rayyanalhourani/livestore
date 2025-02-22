c40787a3d1370a7894fb7d09e61fb5cf1703b7b0
rayyanalhourani <rayyan.alhourani02@gmail.com>
2025-02-22 18:13:48 +0300

    add nullable

--- database/migrations/2025_02_17_065231_create_products_table.php
+++ database/migrations/2025_02_17_065231_create_products_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('slug');
-            $table->text('description');
+            $table->string('slug')->nullable();
+            $table->text('description')->nullable();
             $table->decimal('price', 10, 2);
             $table->unsignedInteger("stock")->default(0);
             $table->timestamps();
