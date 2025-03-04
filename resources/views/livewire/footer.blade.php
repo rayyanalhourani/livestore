2218400b58aec3821f5a557c10382f39bfcf5773
rayyanalhourani <rayyan.alhourani02@gmail.com>
2025-03-04 23:54:32 +0300

    Add footer layout

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -1,8 +1,22 @@
-<div class="mx-20">
-      <div class="grid grid-cols-4 mx-40">
-            <div>123123</div>
-            <div>123123</div>
-            <div>123123</div>
-            <div>123123</div>
+<div class="mx-auto">
+      <div class="grid grid-cols-5">
+            <div class="flex items-center justify-center" >
+                  <h1>Exclusive</h1>
+            </div>
+            <div class="flex items-center justify-center">
+                  <h1>Support</h1>
+            </div>
+            <div class="flex items-center justify-center">
+                  <h1>Account</h1>
+            </div>
+            <div class="flex items-center justify-center">
+                  <h1>Quick Link</h1>
+            </div>
+            <div class="flex items-center justify-center">
+                  <h1>Download App</h1>
+            </div>
+      </div>
+      <div>
+            Copyright Rimel 2022. All right reserved
       </div>
 </div>
