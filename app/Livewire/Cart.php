cbc3004a03fa0b27e3425c5b64cbb640e7912e05
rayyanalhourani <rayyan.alhourani02@gmail.com>
2025-05-02 04:17:56 +0300

    delete cart item

--- app/Livewire/Cart.php
+++ app/Livewire/Cart.php
@@ -8,8 +8,15 @@ use Livewire\Component;
 class Cart extends Component
 {
     public $items;
-    public function mount(){
-        $this->items=Auth::user()->cart->items()->with('product')->get();
+    public function mount()
+    {
+        $this->items = Auth::user()->cart->items()->with('product')->get();
+    }
+
+    public function deleteProductFromCart($productId)
+    {
+        Auth::user()->cart->items()->where('product_id', $productId)->delete();
+        $this->mount();
     }
     public function render()
     {
